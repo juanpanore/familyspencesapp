a2dca3ee9c9d287b1e6f7859d81b2278a9f49724
Jeronimo Quintero Gomez <jeronimoq18@gmail.com>
2025-11-14 10:44:25 -0500

    Se agrega tabla de visualizacion

--- src/app/component/expense/expense.component.ts
+++ src/app/component/expense/expense.component.ts
@@ -12,7 +12,7 @@ export class ExpenseComponent implements OnInit {
     titulo: new FormControl('', [Validators.required, Validators.minLength(3)]),
     descripcion: new FormControl('', [Validators.required, Validators.minLength(5)]),
     periodo: new FormControl('', [Validators.required]),
-    valor: new FormControl('', [Validators.required, Validators.min(0)]),
+    valor: new FormControl('', [Validators.required, Validators.pattern('^[0-9]+$'), Validators.min(0)]),
     categoria: new FormControl('', [Validators.required]),
     responsable: new FormControl('', [Validators.required])
   });
@@ -32,12 +32,29 @@ export class ExpenseComponent implements OnInit {
   }
 
   onSubmit() {
+    console.log('Form submission triggered');
+    console.log('Form valid status:', this.expenseForm.valid);
+    console.log('Form values:', this.expenseForm.value);
+
+    // Log the state of each form control
+    Object.keys(this.expenseForm.controls).forEach(controlName => {
+      const control = this.expenseForm.get(controlName);
+      console.log(`Control: ${controlName}, Value: ${control?.value}, Valid: ${control?.valid}, Errors: ${control?.errors}`);
+    });
+
     if (this.expenseForm.valid) {
       this.expenses.push(this.expenseForm.value);
+      console.log('Expense added:', this.expenseForm.value);
       this.expenseForm.reset();
-      this.showForm = false;
+      this.showForm = false; // Return to the view screen after saving
     } else {
       console.log('Formulario inválido');
     }
+  }
+
+  onValorInput(event: Event): void {
+    const inputElement = event.target as HTMLInputElement;
+    const numericValue = inputElement.value.replace(/[^0-9]/g, '');
+    this.expenseForm.get('valor')?.setValue(numericValue);
   }
 }
