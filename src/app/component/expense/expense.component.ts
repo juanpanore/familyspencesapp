753ccde5b2714b7718b0a3889c106a70a31644f6
Jeronimo Quintero Gomez <jeronimoq18@gmail.com>
2025-11-13 21:07:57 -0500

    Se agrega el html, css y tyscript, ademas del routing

--- src/app/component/expense/expense.component.ts
+++ src/app/component/expense/expense.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, Validators, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-expense',
@@ -7,9 +8,36 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ExpenseComponent implements OnInit {
 
+  expenseForm = new FormGroup({
+    titulo: new FormControl('', [Validators.required, Validators.minLength(3)]),
+    descripcion: new FormControl('', [Validators.required, Validators.minLength(5)]),
+    periodo: new FormControl('', [Validators.required]),
+    valor: new FormControl('', [Validators.required, Validators.min(0)]),
+    categoria: new FormControl('', [Validators.required]),
+    responsable: new FormControl('', [Validators.required])
+  });
+
+  categorias = ['Alimentación', 'Transporte', 'Educación', 'Entretenimiento'];
+  responsables = ['Juan', 'María', 'Pedro', 'Ana'];
+  showForm = false;
+  expenses: any[] = [];
+
   constructor() { }
 
   ngOnInit(): void {
   }
 
+  toggleForm(): void {
+    this.showForm = !this.showForm;
+  }
+
+  onSubmit() {
+    if (this.expenseForm.valid) {
+      this.expenses.push(this.expenseForm.value);
+      this.expenseForm.reset();
+      this.showForm = false;
+    } else {
+      console.log('Formulario inválido');
+    }
+  }
 }
