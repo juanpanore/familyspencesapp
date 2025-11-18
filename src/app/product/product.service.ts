a0b2f9f63fd9c8801168d4c3d3243c389ae44d73
chetos <davidloph>
2025-11-18 16:44:55 -0500

    Se modifica uso del token.

--- src/app/product/product.service.ts
+++ src/app/product/product.service.ts
@@ -1,6 +1,8 @@
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Observable } from 'rxjs';
+import { environment } from '../../environments/environment';
+
 
 export interface Product {
   producto: string;
@@ -13,20 +15,7 @@ export interface Product {
 })
 export class ProductService {
 
-  private apiUrl = 'http://localhost:8080/api/product';
-
-  private token = 'eyJhbGciOiJIUzI1NiJ9.eyJpZFVzZXIiOiJjZGM1NWE1Mi1jMGRkLTRlNzctOWQyNi05Nzg0Yjk3Mzg4MDEiLCJpZEZhbWlseSI6IjQ3ZTJhMTAzLTljNGYtNGNiNi1iOTZiLWI3MTAwOWMyYWQ1MyIsImlhdCI6MTc2MzQzNDk2OSwiZXhwIjoxNzYzNTIxMzY5fQ.nBF4s_94Y_Ww849sSbmIQVfIlXbRDkKEqL3g5ImLeT8';
-
   constructor(private http: HttpClient) {}
-
-  private getHeaders() {
-    return {
-      headers: new HttpHeaders({
-        'Authorization': `Bearer ${this.token}`,
-        'Content-Type': 'application/json'
-      })
-    };
-  }
 
   private baseUrl = 'http://localhost:8080/api/product';
 
@@ -35,11 +24,11 @@ export class ProductService {
     const url = nombre && nombre.trim() !== ''
       ? `${this.baseUrl}?nombre=${nombre}`
       : this.baseUrl;
-    return this.http.get<Product[]>(url, this.getHeaders());
+    return this.http.get<Product[]>(url);
   }
 
   // Agregar un nuevo producto
   addProduct(producto: Product): Observable<any> {
-    return this.http.post(`${this.baseUrl}/product`, producto, this.getHeaders());
+    return this.http.post(`${this.baseUrl}/product`, producto);
   }
 }
