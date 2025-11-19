ae75a9f13cad58a13972a2407c86df41463a148c
Maria Jose <majorr1017@gmail.com>
2025-11-19 18:12:41 -0500

    Se agregran los service, los list y algunos components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { ReactiveFormsModule } from '@angular/forms';  
+import { ReactiveFormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -8,13 +8,23 @@ import { ExpenseComponent } from './component/expense/expense.component';
 import { HttpClientModule } from '@angular/common/http';
 import { ProductComponent } from './product/product.component';
 import {GoalsComponent} from "./goals/goals.component";
+import { BudgetComponent } from './budget/budget';
+import { BudgetListComponent } from './components/budget/budget-list/budget-list.component';
+import { BudgetCreateComponent } from './components/budget/budget-create/budget-create.component';
+import { BudgetDetailsComponent } from './components/budget/budget-details/budget-details.component';
+
+
 
 @NgModule({
   declarations: [
     AppComponent,
     ExpenseComponent,
     ProductComponent,
-    GoalsComponent
+    GoalsComponent,
+    BudgetComponent,
+    BudgetListComponent,
+    BudgetCreateComponent,
+    BudgetDetailsComponent
   ],
   imports: [
     BrowserModule,
