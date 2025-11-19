ebdc72944ae485b37ed65495852802c5a5fab3b2
JuanCarJ <juan.cardenas5279@uco.net.co>
2025-11-19 14:01:26 -0500

    Se arregla app.module para compilacion

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { ReactiveFormsModule } from '@angular/forms';  
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';  
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -8,9 +8,6 @@ import { ExpenseComponent } from './component/expense/expense.component';
 import { HttpClientModule } from '@angular/common/http';
 import { ProductComponent } from './product/product.component';
 import {GoalsComponent} from "./goals/goals.component";
-import { ExpenseComponent } from './expense/expense.component';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [
