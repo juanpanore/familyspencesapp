0468fe05a1c7d4a90013bab0524bf7246f1370cd
jpg27 <juanpablog01p@gmail.com>
2025-11-20 20:11:18 -0500

    front mascotas con id familia quemado

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,31 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ProductComponent } from './product/product.component';
 import {GoalsComponent} from "./goals/goals.component";
 
+import { PetComponent } from './pet/pet.component';
+
 @NgModule({
   declarations: [
     AppComponent,
     ProductComponent,
-    GoalsComponent
+    GoalsComponent,
+    PetComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    CommonModule,  
+    RouterModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
