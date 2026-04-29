385a8c0dc96a46fc34a7c3c571e7ee69ed484d84
Maria Jose <majorr1017@gmail.com>
2026-04-29 01:18:13 -0500

    Cambios budget

--- src/app/service/budget/budget.service.ts
+++ src/app/service/budget/budget.service.ts
@@ -8,45 +8,48 @@ import { AuthService } from 'src/app/services/auth.service';
 })
 export class BudgetService {
 
-  private apiUrl = `http://localhost:8080/api`;
+  private readonly apiUrl = 'http://localhost:8080/api';
 
   constructor(private http: HttpClient, private authService: AuthService) {}
 
-  public getAllBudgetsByFamily(): Observable<any>{
-    const token = this.authService.getToken();
+  private getAuthHeaders(): HttpHeaders {
+    return new HttpHeaders({
+      'Authorization': `Bearer ${this.authService.getToken()}`
+    });
+  }
+
+  public getAllBudgetsByFamily(): Observable<any> {
     const idFamily = this.authService.getFamilyId();
-    const headers = new HttpHeaders({
-      'Authorization': `Bearer ${token}`
-    })
-    return this.http.get(this.apiUrl + "/families/" + idFamily + "/budgets", {headers: headers});
+    return this.http.get(`${this.apiUrl}/families/${idFamily}/budgets`, {
+      headers: this.getAuthHeaders()
+    });
   }
 
-    public getAllFamilies(): Observable<any>{
-      const token = this.authService.getToken();
-      const headers = new HttpHeaders({
-        'Authorization': `Bearer ${token}`
-      })
-    return this.http.get(this.apiUrl + "/v1"+ "/family", {headers: headers});
+  public getAllFamilies(): Observable<any> {
+    return this.http.get(`${this.apiUrl}/v1/family`, {
+      headers: this.getAuthHeaders()
+    });
   }
 
-    public saveBudget(budget: any): Observable<any>{
-      const token = this.authService.getToken();
-      const idFamily = this.authService.getFamilyId();
-      const headers = new HttpHeaders({
-        'Authorization': `Bearer ${token}`
-      })
-      if (!idFamily) {
-        return throwError(() => new Error('No se encontró el id de la familia'));
-      }
-    return this.http.post(this.apiUrl + "/families/" + idFamily + "/budgets", budget , {headers: headers});
+  public saveBudget(budget: any): Observable<any> {
+    const idFamily = this.authService.getFamilyId();
+    if (!idFamily) {
+      return throwError(() => new Error('No se encontró el id de la familia'));
+    }
+    return this.http.post(`${this.apiUrl}/families/${idFamily}/budgets`, budget, {
+      headers: this.getAuthHeaders()
+    });
   }
 
-    public getBudgetDetail(idBuget: any): Observable<any>{
-    const token = this.authService.getToken();
-    const headers = new HttpHeaders({
-      'Authorization': `Bearer ${token}`
-    })
-    return this.http.get(this.apiUrl + "/budgets/" + idBuget , {headers: headers});
+  public getBudgetDetail(idBudget: string): Observable<any> {
+    return this.http.get(`${this.apiUrl}/budgets/${idBudget}`, {
+      headers: this.getAuthHeaders()
+    });
   }
 
+  public deleteBudget(idBudget: string): Observable<any> {
+    return this.http.delete(`${this.apiUrl}/budgets/${idBudget}`, {
+      headers: this.getAuthHeaders()
+    });
+  }
 }
